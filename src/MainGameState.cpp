13cac48f71af401f24701ebce4a54951008e4630
Alex <acl144@alu.ua.es>
2026-09-10 10:19:23 +0200

    Práctica 0: Ejercicio 0

--- src/MainGameState.cpp
+++ src/MainGameState.cpp
@@ -1,6 +1,10 @@
 #include <MainGameState.hpp>
 #include <iostream>
 
+extern "C" {
+    #include <raylib.h>
+}
+
 MainGameState::MainGameState()
 {
 }
@@ -22,5 +26,8 @@ void MainGameState::update(float deltaTime)
 
 void MainGameState::render()
 {
-
+    BeginDrawing();
+    ClearBackground(WHITE);
+    DrawText("Bienvenido a Flappy Bird DCA.", 10, 10, 20, BLACK);
+    EndDrawing();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <memory>
 #include <chrono>
 
+extern "C" {
+    #include <raylib.h>
+}
+
 int main()
 { 
     float delta_time = 0.0f;
@@ -10,6 +14,8 @@ int main()
     StateMachine state_machine = StateMachine();
     state_machine.add_state(std::make_unique<MainGameState>(), false);
     state_machine.handle_state_changes(delta_time);
+
+    InitWindow(288, 512, "Flapy Bird");
 
     while (!state_machine.is_game_ending())
     {
